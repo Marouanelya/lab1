fbd95e15599b0b0f61494d46868c5a76a0dca157
E.Marouane <marouanelya@gmail.com>
2017-03-13 17:02:32 +0100

    3eme Commit

--- src/main/java/com/polytech/business/PublicationService.java
+++ src/main/java/com/polytech/business/PublicationService.java
@@ -20,8 +20,6 @@ public class PublicationService {
 
     public void post(Post post){
         postRepository.save(post);
-        //hhhhhhh
-        //Hello World
     }
 
     public List<Post> fetchAll(){
